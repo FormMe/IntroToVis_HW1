e16e9d3ab06656a2f5ee5b4fa82f54ea031af972
FormMe <formme1995@yandex.ru>
2017-10-16 00:45:27 +0300

    fixed sorting

--- hw2/tableScript.js
+++ hw2/tableScript.js
@@ -46,10 +46,27 @@ var format_data = function(row){
       .append("th")
       .text(function(d) { return d;})
       .on("click", function(header, i) {
+ 		ascending = !ascending;
         tbody.selectAll("tr")
 			 .sort(function(a, b) {
-					  return d3.descending(a[header], b[header]) || d3.ascending(a["name"], b["name"]);
-					});
+ 		console.log(header);
+			 		if(ascending) {
+			 			if(header == "continent") {
+					  		return d3.ascending(a[header], b[header]) || d3.ascending(a["name"], b["name"]);
+						}
+						else{
+							return d3.ascending(a[header], b[header]);
+						}
+			 		}
+			 		else {
+			 			if(header == "continent") {
+					  		return d3.descending(a[header], b[header]) || d3.ascending(a["name"], b["name"]);
+						}
+						else {
+							return d3.descending(a[header], b[header]);
+						}
+			 		}
+				});
 			zebra_color();
       });
 	
@@ -167,3 +184,103 @@ var format_data = function(row){
 			zebra_color();		    		
       })
 	});
+
+var categories= ['','Accessories', 'Audiophile', 'Camera & Photo', 'Cell Phones', 'Computers','eBook Readers','Gadgets','GPS & Navigation','Home Audio','Office Electronics','Portable Audio','Portable Video','Security & Surveillance','Service','Television & Video','Car & Vehicle'];
+
+		var dollars = [213,209,190,179,156,209,190,179,213,209,190,179,156,209,190,190];
+
+		var colors = ['#0000b4','#0082ca','#0094ff','#0d4bcf','#0066AE','#074285','#00187B','#285964','#405F83','#416545','#4D7069','#6E9985','#7EBC89','#0283AF','#79BCBF','#99C19E'];
+
+		var grid = d3.range(25).map(function(i){
+			return {'x1':0,'y1':0,'x2':0,'y2':480};
+		});
+
+		var tickVals = grid.map(function(d,i){
+			if(i>0){ return i*10; }
+			else if(i===0){ return "100";}
+		});
+
+		var xscale = d3.scaleLinear()
+						.domain([10,250])
+						.range([0,722]);
+
+		var yscale = d3.scaleLinear()
+						.domain([0,categories.length])
+						.range([0,480]);
+
+		var colorScale = d3.scaleQuantize()
+						.domain([0,categories.length])
+						.range(colors);
+
+		var canvas = d3.select('#wrapper')
+						.append('svg')
+						.attr('width',900)
+						.attr('height',550);
+
+		var grids = canvas.append('g')
+						  .attr('id','grid')
+						  .attr('transform','translate(150,10)')
+						  .selectAll('line')
+						  .data(grid)
+						  .enter()
+						  .append('line')
+						  .attrs({'x1':function(d,i){ return i*30; },
+								 'y1':function(d){ return d.y1; },
+								 'x2':function(d,i){ return i*30; },
+								 'y2':function(d){ return d.y2; },
+							})
+						  .style({'stroke':'#adadad','stroke-width':'1px'});
+
+		/*var	xAxis = d3.svg.axis();
+			xAxis
+				.orient('bottom')
+				.scale(xscale)
+				.tickValues(tickVals);
+
+		var	yAxis = d3.svg.axis();
+			yAxis
+				.orient('left')
+				.scale(yscale)
+				.tickSize(2)
+				.tickFormat(function(d,i){ return categories[i]; })
+				.tickValues(d3.range(17));*/
+
+		var y_xis = canvas.append('g')
+						  .attr("transform", "translate(150,0)")
+						  .attr('id','yaxis')
+						  .call(d3.axisLeft(yscale));
+
+		var x_xis = canvas.append('g')
+						  .attr("transform", "translate(150,480)")
+						  .attr('id','xaxis')
+						  .call(d3.axisBottom(xscale));
+
+		var chart = canvas.append('g')
+							.attr("transform", "translate(150,0)")
+							.attr('id','bars')
+							.selectAll('rect')
+							.data(dollars)
+							.enter()
+							.append('rect')
+							.attr('height',19)
+							.text(function (d, i) { return categories[i]; })
+							.attrs({'x':0,'y':function(d,i){ return yscale(i)+19; }})
+							.style('fill',function(d,i){ return colorScale(i); })
+							.attr('width',function(d){ return 0; });
+
+
+		var transit = d3.select("svg").selectAll("rect")
+						    .data(dollars)
+						    .transition()
+						    .duration(1000) 
+						    .attr("width", function(d) {return xscale(d); });
+
+		var transitext = d3.select('#bars')
+							.selectAll('text')
+							.data(dollars)
+							.enter()
+							.append('text')
+							.attrs({'x':function(d) {return xscale(d)-200; },'y':function(d,i){ return yscale(i)+35; }})
+							.text(function(d, i){ return categories[i] + d+"$"; })
+							.style({'fill':'#fff','font-size':'14px'});
+
