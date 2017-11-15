17524246a82a7a443c1d36ae08e0a210929888eb
FormMe <formme1995@yandex.ru>
2017-11-16 02:03:03 +0300

    convinient coordinates updator

--- hw4_1/js/script.js
+++ hw4_1/js/script.js
@@ -1,13 +1,25 @@
 
  d3.json("https://raw.githubusercontent.com/avt00/dvcourse/master/countries_2012.json", function(error, data){
 
+	d3.selectAll('input[name="Mode"]')
+  	  .on("change", function(){
+			var mode = d3.select('input[name="Mode"]:checked').node().value;
+
+	});
+
+
 	var margin = {top: 50, bottom: 10, left: 300, right: 40};
 	var width = 900 - margin.left - margin.right;
 	var height = 1500 - margin.top - margin.bottom;
 	var nodeRadius = 5; 
-	var svg = d3.select("#first")
+	var svg = d3.select("#plot")
 	            .attr("width", width)
 	            .attr("height", height);
+
+    data.forEach(function (d, i) {
+    	d.x = 10; d.y = 0;
+    })
+    console.log(data);
 	var nodes = svg.selectAll('.node')
 	              .data(data)
 	            .enter()
@@ -24,47 +36,49 @@
         .attr('font-size', '10px')
         .text(function(d) { return d.name; });
 
+    function ranking_coords(rank){
+    	var yScale;
+    	if (rank !='No'){
+			yScale = d3.scaleLinear()
+				.range([height - 10, 0])
+				.domain([d3.min(data, d => d[rank]), d3.max(data, d => d[rank])]);
+		}
+    	data.forEach(function (d, i) {
+	  		d.y = rank =='No'? 5 + 2*i*nodeRadius
+	  						 : 5 + yScale(d[rank]);	  		
+    	})
+  		
+    }
 
-    var update = function (rank) {
+    var update = function () {
 		nodes.transition()
 		  	.duration(500)
 	      	.attr("transform", function(d, i) { 
-		      		var y;
-		      		if(rank =='No') {
-		      			y = 5 + 2*i*nodeRadius;
-		      		}
-		      		else {	      			
-						var yScale = d3.scaleLinear()
-							.range([height - 10, 0])
-							.domain([d3.min(data, d => d[rank]), d3.max(data, d => d[rank])]);
-		      			y = 5 + yScale(d[rank]);
-		      		}
-			        return "translate(10,"+ y +")"; 
+			        return "translate("+ d.x + "," + d.y +")"; 
 			      });  
     }
 
-    update('No');
+	ranking_coords('No');
+    update();
+
+    function ranking() {
+		var Ranking = d3.select('input[name="Ranking"]:checked').node().value;
+		if (Ranking == 'No') {
+			ranking_coords('No');
+		}
+		else{
+	  			var rank = d3.select("#Rank").node().value; 
+			ranking_coords(rank);	
+		}
+		update();
+    }
 
 	d3.selectAll('input[name="Ranking"]')
   	  .on("change", function(){
-			var Ranking = d3.select('input[name="Ranking"]:checked').node().value;
-			if (Ranking == 'No') {
-				update('No');
-			}
-			else{
-  	  			var rank = d3.select("#Rank").node().value; 
-				update(rank);	
-			}
+			ranking();
 	});
 	d3.selectAll('#Rank')
   	  .on("change", function(){
-			var Ranking = d3.select('input[name="Ranking"]:checked').node().value;
-			if (Ranking == 'No') {
-				update('No');
-			}
-			else{
-  	  			var rank = d3.select("#Rank").node().value; 
-				update(rank);	
-			}
+			ranking();
 	});
 });
