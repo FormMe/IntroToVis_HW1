39352fe5deafa5e65b67ade055de76ad95a9334a
FormMe <formme1995@yandex.ru>
2017-11-16 22:16:20 +0300

    force layout

--- hw4_1/js/script.js
+++ hw4_1/js/script.js
@@ -11,6 +11,8 @@
 				case 'R': ranking_layout(); break;
 				case 'S': scatter_layout(); break;
 				case 'C': circular_layout(); break;
+				case 'G': force_layout(); break;
+
 			}
  	}
 
@@ -48,11 +50,12 @@
 
   
     function ranking_layout() {
+  		force.stop();
 		var Ranking = d3.select('input[name="Ranking"]:checked').node().value;
 		var rank = (Ranking == 'No') ? 'No' : d3.select("#Rank").node().value; 
 		var yScale;
 		if (Ranking != 'No')
-			yScale = d3.scaleLinear()
+			yScale = d3.scale.linear()
 				.range([height - 15, 10])
 				.domain([d3.min(data, d => d[rank]), d3.max(data, d => d[rank])]);
 		
@@ -61,19 +64,20 @@
 		  		d.y = rank =='No'? 5 + 2*i*nodeRadius
 		  						 : 5 + yScale(d[rank]);	  		
 	    	})
-		update();
+		update(500);
     }
     function scatter_layout(){
+  		force.stop();
     	var mode = d3.select('input[name="Coordinates"]:checked').node().value;
     	var xAxis = mode == 'lon/lat' ? 'longitude': 'population';
     	var yAxis = mode == 'lon/lat' ? 'latitude': 'gdp';
-    	var xScale = d3.scaleLinear()
-				.range([10, width-70])
+    	var xScale = d3.scale.linear()
+				.range([20, width-70])
 				.domain([d3.min(data, d => d[xAxis]),
 						 d3.max(data, d => d[xAxis])]);
 
-    	var yScale = d3.scaleLinear()
-				.range([height-600, 10])
+    	var yScale = d3.scale.linear()
+				.range([height-600, 20])
 				.domain([d3.min(data, d => d[yAxis]),
 						 d3.max(data, d => d[yAxis])]);
 
@@ -81,15 +85,16 @@
 	  		d.x = xScale(d[xAxis]);	  	
 	  		d.y = yScale(d[yAxis]);	  	
     	});  		
-		update();
+		update(500);
     }
 	function circular_layout() {
-	  var r = Math.min(height, width)/2 - 50;
+  	  force.stop();
+	  var r = Math.min(height, width)/2 - 350;
 
-	  var arc = d3.arc()
+	  var arc = d3.svg.arc()
 	          .outerRadius(r);
 	  var p = d3.select("#Sort").node().value;
-	  var pie = d3.pie()
+	  var pie = d3.layout.pie()
 				  .sort(function(a, b) { return a[p] - b[p];})
 			          .value(function(d, i) { 
 			            return 1; 
@@ -101,17 +106,31 @@
 	    d.outerRadius = r;
 
 	    // Building the data object we are going to return
-	    d.data.x = arc.centroid(d)[0]+width/2;
-	    d.data.y = arc.centroid(d)[1]+height/2;
+	    d.data.x = arc.centroid(d)[0] + r + 50;
+	    d.data.y = arc.centroid(d)[1] + r + 50;
 
 	    return d.data;
 	  })
-	  update();
+	  update(700);
 	}
 
-    function update() {
+	var force = d3.layout.force()
+	    .size([width, height])
+	    .charge(-50)
+	    .linkDistance(10)
+	    .on("tick", function(d) {update(150);})
+	    .on("start", function(d) {})
+	    .on("end", function(d) {});
+
+	function force_layout() {
+	 force.nodes(data)
+	      .start();
+	}
+
+
+    function update(duration) {
 		nodes.transition()
-		  	.duration(500)
+		  	.duration(duration)
 	      	.attr("transform", function(d, i) { 
 			        return "translate("+ d.x + "," + d.y +")"; 
 			      });  
@@ -122,7 +141,7 @@
 	layout_mode();
 
 	d3.selectAll('input[name="Ranking"]').on("change",ranking_layout);
-	d3.selectAll('#Rank').on("change", ranking_layout);
 	d3.selectAll('input[name="Coordinates"]').on("change", scatter_layout);
+	d3.selectAll('#Rank').on("change", ranking_layout);
 	d3.selectAll('#Sort').on("change", circular_layout);
 });
